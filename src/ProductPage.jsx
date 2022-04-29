6f4c8ef88571515b8f86b99e228cff86be386443
sbmsr <messs@seas.upenn.edu>
2022-04-29 16:56:22 -0400

    moved products to db.json

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,42 +1,15 @@
-import React from 'react';
-
-const products = [
-  {
-    id: 1,
-    name: 'Earthen Bottle',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-  },
-  {
-    id: 2,
-    name: 'Nomad Tumbler',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-    imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-  },
-  {
-    id: 3,
-    name: 'Focus Paper Refill',
-    href: '#',
-    price: '$89',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-  },
-  {
-    id: 4,
-    name: 'Machined Mechanical Pencil',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-  },
-  // More products...
-]
+import React, { useEffect, useState } from "react";
 
 export default function ProductPage() {
+  let [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    if (products.length > 0) return;
+    let res = await fetch("http://localhost:3001/products");
+    let body = await res.json();
+    setProducts(body);
+  });
+
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -59,6 +32,5 @@ export default function ProductPage() {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
