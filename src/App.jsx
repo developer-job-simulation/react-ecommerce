dcc1d2b12098dfc113de8a2e4262c99c9ebc8621
sbmsr <messs@seas.upenn.edu>
2022-04-29 16:22:40 -0400

    product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ProductPage from './ProductPage';
 
 function App() {
   return (
     <main>
       Hello World
+      <ProductPage />
     </main>
-
   );
 }
 
