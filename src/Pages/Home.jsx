e101207921bb5b785cacebdcafc6b47f230f6853
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-05-09 20:05:13 +1000

    cart size in nav shows accurately

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import Cart from "../Components/Cart";
 import NavBar from "../Components/NavBar";
 import ProductTable from "../Components/ProductTable";
 
 function Home() {
-  const [open, setOpen] = useState(false);
-  const [cart, updateCart] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [cart, updateCart] = useState([]);
+	const [cartSize, setCartSize] = useState(0);
 
-  return (
-    <main>
-      <NavBar {...{ setOpen }} />
-      <Cart {...{ open, setOpen, cart, updateCart }} />
-      <ProductTable {...{ cart, updateCart }} />
-    </main>
-  );
+	useEffect(() => {
+		let cartSizeCount = 0;
+
+		// runs through the cart array, counting the amount of items
+		cart.forEach((product) => {
+			cartSizeCount += product.quantity;
+		});
+
+		setCartSize(cartSizeCount);
+	}, [cart]);
+
+	return (
+		<main>
+			<NavBar {...{ setOpen, cartSize }} />
+			<Cart {...{ open, setOpen, cart, updateCart }} />
+			<ProductTable {...{ cart, updateCart }} />
+		</main>
+	);
 }
 
 export default Home;
