3b2279c93fdb814ef571816564b2e21f7895700e
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-05-09 18:51:29 +1000

    sort functionality implementation

--- src/Components/ProductFilters.jsx
+++ src/Components/ProductFilters.jsx
@@ -3,131 +3,169 @@ import { ChevronDownIcon, FilterIcon } from "@heroicons/react/solid";
 import React, { Fragment } from "react";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+	return classes.filter(Boolean).join(" ");
 }
 
-export default function ProductFilters({ filterOptions, setFilterOptions, sortOptions, setSortOptions }) {
-  return (
-    <Disclosure
-      as="section"
-      aria-labelledby="filter-heading"
-      className="relative z-10  border-gray-200 grid items-center"
-    >
-      <h2 id="filter-heading" className="sr-only">
-        Filters
-      </h2>
-      <div className="relative col-start-1 row-start-1 py-4">
-        <div className="max-w-7xl mx-auto flex space-x-6 divide-x divide-gray-200 text-sm px-4 sm:px-6 lg:px-8">
-          <div>
-            <Disclosure.Button className="group text-gray-700 font-medium flex items-center">
-              <FilterIcon
-                className="flex-none w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500"
-                aria-hidden="true"
-              />
-              0 Filters
-            </Disclosure.Button>
-          </div>
-          <div className="pl-6">
-            <button type="button" className="text-gray-500">
-              Clear all
-            </button>
-          </div>
-        </div>
-      </div>
-      <Disclosure.Panel className="border-gray-200 py-10">
-        <div className="max-w-7xl mx-auto grid grid-cols-2 gap-x-4 px-4 text-sm sm:px-6 md:gap-x-6 lg:px-8">
-          <div className="grid grid-cols-1 gap-y-10 auto-rows-min md:grid-cols-2 md:gap-x-6">
-            <fieldset>
-              <legend className="block font-medium">Price</legend>
-              <div className="pt-6 space-y-6 sm:pt-4 sm:space-y-4">
-                {filterOptions.price.map((option, optionIdx) => (
-                  <div key={option.minValue} className="flex items-center text-base sm:text-sm">
-                    <input
-                      id={`price-${optionIdx}`}
-                      name="price[]"
-                      defaultValue={option.minValue}
-                      type="checkbox"
-                      className="flex-shrink-0 h-4 w-4 border-gray-300 rounded text-black focus:ring-black"
-                      defaultChecked={option.checked}
-                    />
-                    <label htmlFor={`price-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-600">
-                      {option.label}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </fieldset>
-            <fieldset>
-              <legend className="block font-medium">Color</legend>
-              <div className="pt-6 space-y-6 sm:pt-4 sm:space-y-4">
-                {filterOptions.color.map((option, optionIdx) => (
-                  <div key={option.value} className="flex items-center text-base sm:text-sm">
-                    <input
-                      id={`color-${optionIdx}`}
-                      name="color[]"
-                      defaultValue={option.value}
-                      type="checkbox"
-                      className="flex-shrink-0 h-4 w-4 border-gray-300 rounded text-black focus:ring-black"
-                      defaultChecked={option.checked}
-                    />
-                    <label htmlFor={`color-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-600">
-                      {option.label}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </fieldset>
-          </div>
-        </div>
-      </Disclosure.Panel>
-      <div className="col-start-1 row-start-1 py-4">
-        <div className="flex justify-end max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <Menu as="div" className="relative inline-block">
-            <div className="flex">
-              <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                Sort
-                <ChevronDownIcon
-                  className="flex-shrink-0 -mr-1 ml-1 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                  aria-hidden="true"
-                />
-              </Menu.Button>
-            </div>
+export default function ProductFilters({
+	filterOptions,
+	setFilterOptions,
+	sortOptions,
+	setSortOptions,
+	sortProducts,
+}) {
+	// toggles the selected option and sets the other option to false
+	// sortOption is the object of the selected sorting option
+	const handleSort = (sortOption) => {
+		let sortOptionsCopy = [...sortOptions];
 
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="py-1">
-                  {sortOptions.map((option) => (
-                    <Menu.Item key={option.name}>
-                      {({ active }) => (
-                        <button
-                          onClick={() => {
-                            // TODO
-                          }}
-                          className={classNames(
-                            option.current ? "font-medium text-gray-900" : "text-gray-500",
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm"
-                          )}
-                        >
-                          {option.name}
-                        </button>
-                      )}
-                    </Menu.Item>
-                  ))}
-                </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </div>
-      </div>
-    </Disclosure>
-  );
+		// find selected option, toggle it and set the other options to false
+		for (let i = 0; i < sortOptionsCopy.length; i++) {
+			if (sortOptionsCopy[i].name == sortOption.name) {
+				sortOptionsCopy[i].current = !sortOptionsCopy[i].current;
+			} else {
+				sortOptionsCopy[i].current = false;
+			}
+		}
+
+		setSortOptions([...sortOptionsCopy]);
+	};
+
+	return (
+		<Disclosure
+			as="section"
+			aria-labelledby="filter-heading"
+			className="relative z-10  border-gray-200 grid items-center"
+		>
+			<h2 id="filter-heading" className="sr-only">
+				Filters
+			</h2>
+			<div className="relative col-start-1 row-start-1 py-4">
+				<div className="max-w-7xl mx-auto flex space-x-6 divide-x divide-gray-200 text-sm px-4 sm:px-6 lg:px-8">
+					<div>
+						<Disclosure.Button className="group text-gray-700 font-medium flex items-center">
+							<FilterIcon
+								className="flex-none w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500"
+								aria-hidden="true"
+							/>
+							0 Filters
+						</Disclosure.Button>
+					</div>
+					<div className="pl-6">
+						<button type="button" className="text-gray-500">
+							Clear all
+						</button>
+					</div>
+				</div>
+			</div>
+			<Disclosure.Panel className="border-gray-200 py-10">
+				<div className="max-w-7xl mx-auto grid grid-cols-2 gap-x-4 px-4 text-sm sm:px-6 md:gap-x-6 lg:px-8">
+					<div className="grid grid-cols-1 gap-y-10 auto-rows-min md:grid-cols-2 md:gap-x-6">
+						<fieldset>
+							<legend className="block font-medium">Price</legend>
+							<div className="pt-6 space-y-6 sm:pt-4 sm:space-y-4">
+								{filterOptions.price.map((option, optionIdx) => (
+									<div
+										key={option.minValue}
+										className="flex items-center text-base sm:text-sm"
+									>
+										<input
+											id={`price-${optionIdx}`}
+											name="price[]"
+											defaultValue={option.minValue}
+											type="checkbox"
+											className="flex-shrink-0 h-4 w-4 border-gray-300 rounded text-black focus:ring-black"
+											defaultChecked={option.checked}
+										/>
+										<label
+											htmlFor={`price-${optionIdx}`}
+											className="ml-3 min-w-0 flex-1 text-gray-600"
+										>
+											{option.label}
+										</label>
+									</div>
+								))}
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend className="block font-medium">Color</legend>
+							<div className="pt-6 space-y-6 sm:pt-4 sm:space-y-4">
+								{filterOptions.color.map((option, optionIdx) => (
+									<div
+										key={option.value}
+										className="flex items-center text-base sm:text-sm"
+									>
+										<input
+											id={`color-${optionIdx}`}
+											name="color[]"
+											defaultValue={option.value}
+											type="checkbox"
+											className="flex-shrink-0 h-4 w-4 border-gray-300 rounded text-black focus:ring-black"
+											defaultChecked={option.checked}
+										/>
+										<label
+											htmlFor={`color-${optionIdx}`}
+											className="ml-3 min-w-0 flex-1 text-gray-600"
+										>
+											{option.label}
+										</label>
+									</div>
+								))}
+							</div>
+						</fieldset>
+					</div>
+				</div>
+			</Disclosure.Panel>
+			<div className="col-start-1 row-start-1 py-4">
+				<div className="flex justify-end max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+					<Menu as="div" className="relative inline-block">
+						<div className="flex">
+							<Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+								Sort
+								<ChevronDownIcon
+									className="flex-shrink-0 -mr-1 ml-1 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+									aria-hidden="true"
+								/>
+							</Menu.Button>
+						</div>
+
+						<Transition
+							as={Fragment}
+							enter="transition ease-out duration-100"
+							enterFrom="transform opacity-0 scale-95"
+							enterTo="transform opacity-100 scale-100"
+							leave="transition ease-in duration-75"
+							leaveFrom="transform opacity-100 scale-100"
+							leaveTo="transform opacity-0 scale-95"
+						>
+							<Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+								<div className="py-1">
+									{sortOptions.map((option) => (
+										<Menu.Item key={option.name}>
+											{({ active }) => (
+												<button
+													onClick={() => {
+														handleSort(option);
+														sortProducts();
+													}}
+													className={classNames(
+														option.current
+															? "font-medium text-gray-900"
+															: "text-gray-500",
+														active ? "bg-gray-100" : "",
+														"block px-4 py-2 text-sm"
+													)}
+												>
+													{option.name}
+												</button>
+											)}
+										</Menu.Item>
+									))}
+								</div>
+							</Menu.Items>
+						</Transition>
+					</Menu>
+				</div>
+			</div>
+		</Disclosure>
+	);
 }
--- src/Components/ProductTable.jsx
+++ src/Components/ProductTable.jsx
@@ -2,88 +2,141 @@ import React, { useEffect, useState } from "react";
 import ProductFilters from "./ProductFilters";
 
 const getDefaultFilterOptions = () => {
-  return {
-    price: [
-      { minValue: 0, maxValue: 25, label: "$0 - $25", checked: false },
-      { minValue: 25, maxValue: 50, label: "$25 - $50", checked: false },
-      { minValue: 50, maxValue: 75, label: "$50 - $75", checked: false },
-      { minValue: 75, maxValue: Number.MAX_VALUE, label: "$75+", checked: false },
-    ],
-    color: [
-      { value: "beige", label: "Beige", checked: false },
-      { value: "green", label: "Green", checked: false },
-      { value: "white", label: "White", checked: false },
-      { value: "black", label: "Black", checked: false },
-      { value: "gray", label: "Gray", checked: false },
-      { value: "teal", label: "Teal", checked: false },
-    ],
-  };
+	return {
+		price: [
+			{ minValue: 0, maxValue: 25, label: "$0 - $25", checked: false },
+			{ minValue: 25, maxValue: 50, label: "$25 - $50", checked: false },
+			{ minValue: 50, maxValue: 75, label: "$50 - $75", checked: false },
+			{
+				minValue: 75,
+				maxValue: Number.MAX_VALUE,
+				label: "$75+",
+				checked: false,
+			},
+		],
+		color: [
+			{ value: "beige", label: "Beige", checked: false },
+			{ value: "green", label: "Green", checked: false },
+			{ value: "white", label: "White", checked: false },
+			{ value: "black", label: "Black", checked: false },
+			{ value: "gray", label: "Gray", checked: false },
+			{ value: "teal", label: "Teal", checked: false },
+		],
+	};
 };
 
 const getDefaultSortOptions = () => {
-  return [
-    { name: "Price", current: false },
-    { name: "Newest", current: false },
-  ];
+	return [
+		{ name: "Price", current: false },
+		{ name: "Newest", current: false },
+	];
 };
 
 export default function ProductTable({ cart, updateCart }) {
-  let [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-  const [filterOptions, setFilterOptions] = useState(getDefaultFilterOptions());
-  const [sortOptions, setSortOptions] = useState(getDefaultSortOptions());
+	const [filterOptions, setFilterOptions] = useState(getDefaultFilterOptions());
+	const [sortOptions, setSortOptions] = useState(getDefaultSortOptions());
 
-  useEffect(async () => {
-    console.info("Fetching Products...");
-    let res = await fetch("http://localhost:3001/products");
-    let body = await res.json();
-    setProducts(body);
-  });
+	useEffect(async () => {
+		console.info("Fetching Products...");
+		let res = await fetch("http://localhost:3001/products");
+		let body = await res.json();
+		setProducts(body);
+	}, []);
 
-  return (
-    <div className="bg-white">
-      <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-        <ProductFilters {...{ filterOptions, setFilterOptions, sortOptions, setSortOptions }} />
+	const sortProducts = () => {
+		// finds the indexes of the respective sort types
+		let indexOfPrice = sortOptions
+			.map((object) => object.name)
+			.indexOf("Price");
+		let indexOfNewest = sortOptions
+			.map((object) => object.name)
+			.indexOf("Newest");
 
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="w-full h-full object-center object-cover"
-                />
-                <button
-                  type="button"
-                  className="hidden group-hover:block group-hover:opacity-50 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-black"
-                  onClick={() => {
-                    let newCart = cart.slice();
+		// if sort by price is selected
+		if (sortOptions[indexOfPrice].current) {
+			// sorts products by price into descending order
+			setProducts(
+				products.sort((a, b) => {
+					return b.price - a.price;
+				})
+			);
+		}
+		// sort by newest
+		else if (sortOptions[indexOfNewest].current) {
+			// sorts products based on their release date, descending order
+			setProducts(
+				products.sort((a, b) => {
+					return b.releaseDate - a.releaseDate;
+				})
+			);
+		}
+		// default sort
+		else {
+			// sorts products based on their id's given on the first fetch of data
+			setProducts(
+				products.sort((a, b) => {
+					return a.id - b.id;
+				})
+			);
+		}
+	};
 
-                    if (!newCart.includes(product)) {
-                      product.quantity = 1;
-                      newCart.push(product);
-                    } else {
-                      newCart.map((p) => {
-                        if (p.id === product.id) {
-                          p.quantity += 1;
-                        }
-                      });
-                    }
+	return (
+		<div className="bg-white">
+			<div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+				<h2 className="sr-only">Products</h2>
+				<ProductFilters
+					{...{
+						filterOptions,
+						setFilterOptions,
+						sortOptions,
+						setSortOptions,
+						sortProducts,
+					}}
+				/>
 
-                    updateCart(newCart);
-                  }}
-                >
-                  Add To Cart
-                </button>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+				<div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+					{products.map((product) => (
+						<a key={product.id} className="group">
+							<div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+								<img
+									src={product.imageSrc}
+									alt={product.imageAlt}
+									className="w-full h-full object-center object-cover"
+								/>
+								<button
+									type="button"
+									className="hidden group-hover:block group-hover:opacity-50 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-black"
+									onClick={() => {
+										let newCart = cart.slice();
+
+										if (!newCart.includes(product)) {
+											product.quantity = 1;
+											newCart.push(product);
+										} else {
+											newCart.map((p) => {
+												if (p.id === product.id) {
+													p.quantity += 1;
+												}
+											});
+										}
+
+										updateCart(newCart);
+									}}
+								>
+									Add To Cart
+								</button>
+							</div>
+							<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+							<p className="mt-1 text-lg font-medium text-gray-900">
+								${product.price}
+							</p>
+						</a>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
