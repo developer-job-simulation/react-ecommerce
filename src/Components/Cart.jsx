0e13f706e1fe3b9e7965680576214bef1af471ec
sbmsr <messs@seas.upenn.edu>
2022-05-10 17:29:57 -0400

    Fix cart subtotal to accurately reflect items in cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,7 +2,19 @@ import { Dialog, Transition } from "@headlessui/react";
 import { ShoppingCartIcon, XIcon } from "@heroicons/react/outline";
 import React, { Fragment } from "react";
 
+const currencyFormatter = Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+function calculateSubtotal(cart) {
+  let subtotal = 0;
+  cart.map((item) => (subtotal += item.price * item.quantity));
+  return currencyFormatter.format(subtotal);
+}
+
 export default function Cart({ open, setOpen, cart, updateCart }) {
+  let subtotal = calculateSubtotal(cart);
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -115,7 +127,7 @@ export default function Cart({ open, setOpen, cart, updateCart }) {
                   <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
                     <div className="flex justify-between text-base font-medium text-gray-900">
                       <p>Subtotal</p>
-                      <p>$262.00</p>
+                      <p>{subtotal}</p>
                     </div>
                     <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                     <div className="mt-6">
