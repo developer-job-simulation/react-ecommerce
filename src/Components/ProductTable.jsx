3a4166d27c8c59816c1ff59a62b9283e9f20baf4
Animan Amit <animanamit@wustl.edu>
2022-05-10 23:50:58 +0800

    fixed product fetch loop

--- src/Components/ProductTable.jsx
+++ src/Components/ProductTable.jsx
@@ -7,7 +7,12 @@ const getDefaultFilterOptions = () => {
       { minValue: 0, maxValue: 25, label: "$0 - $25", checked: false },
       { minValue: 25, maxValue: 50, label: "$25 - $50", checked: false },
       { minValue: 50, maxValue: 75, label: "$50 - $75", checked: false },
-      { minValue: 75, maxValue: Number.MAX_VALUE, label: "$75+", checked: false },
+      {
+        minValue: 75,
+        maxValue: Number.MAX_VALUE,
+        label: "$75+",
+        checked: false,
+      },
     ],
     color: [
       { value: "beige", label: "Beige", checked: false },
@@ -38,26 +43,28 @@ export default function ProductTable({ cart, updateCart }) {
     let res = await fetch("http://localhost:3001/products");
     let body = await res.json();
     setProducts(body);
-  });
+  }, []);
 
   return (
     <div className="bg-white">
-      <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="max-w-2xl px-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
-        <ProductFilters {...{ filterOptions, setFilterOptions, sortOptions, setSortOptions }} />
+        <ProductFilters
+          {...{ filterOptions, setFilterOptions, sortOptions, setSortOptions }}
+        />
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <a key={product.id} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <div className="w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={product.imageSrc}
                   alt={product.imageAlt}
-                  className="w-full h-full object-center object-cover"
+                  className="object-cover object-center w-full h-full"
                 />
                 <button
                   type="button"
-                  className="hidden group-hover:block group-hover:opacity-50 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-black"
+                  className="hidden text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm group-hover:block group-hover:opacity-50"
                   onClick={() => {
                     let newCart = cart.slice();
 
@@ -79,7 +86,9 @@ export default function ProductTable({ cart, updateCart }) {
                 </button>
               </div>
               <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                ${product.price}
+              </p>
             </a>
           ))}
         </div>
