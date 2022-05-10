aedcaa71cbe0e779fa7aabea5bf23952710a98f4
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-05-10 13:05:00 +1000

    cart is persistent on refresh

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cart from "../Components/Cart";
 import NavBar from "../Components/NavBar";
 import ProductTable from "../Components/ProductTable";
 
 function Home() {
-  const [open, setOpen] = useState(false);
-  const [cart, updateCart] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [cart, updateCart] = useState([]);
 
-  return (
-    <main>
-      <NavBar {...{ setOpen }} />
-      <Cart {...{ open, setOpen, cart, updateCart }} />
-      <ProductTable {...{ cart, updateCart }} />
-    </main>
-  );
+	useEffect(() => {
+		// on render, fetch the saved cart from local storage
+		const savedCart = localStorage.getItem("cart");
+
+		updateCart(JSON.parse(savedCart));
+	}, []);
+
+	useEffect(() => {
+		// if the cart has been updated after intialization, save to local storage
+		if (cart.length >= 0) {
+			localStorage.setItem("cart", JSON.stringify([...cart]));
+		}
+	}, [cart]);
+
+	return (
+		<main>
+			<NavBar {...{ setOpen }} />
+			<Cart {...{ open, setOpen, cart, updateCart }} />
+			<ProductTable {...{ cart, updateCart }} />
+		</main>
+	);
 }
 
 export default Home;
