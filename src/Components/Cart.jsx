1534ba66b3681f93758e898e667e7105ecff7571
Nick Yu <rednaxela64@outlook.com>
2022-05-18 20:17:32 -0700

    Cart will now display empty cart message when no items have been added to cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,5 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { XIcon } from "@heroicons/react/outline";
+//import { XIcon } from "@heroicons/react/outline";
+import { ShoppingCartIcon, XIcon } from "@heroicons/react/outline";
 import React, { Fragment } from "react";
 
 export default function Cart({ open, setOpen, cart, updateCart }) {
@@ -51,6 +52,14 @@ export default function Cart({ open, setOpen, cart, updateCart }) {
                         </button>
                       </div>
                     </div>
+
+                    {cart.length === 0 && (
+                      <div className="flex h-full flex-col justify-center items-center">
+                        <ShoppingCartIcon className="w-12" />
+                        <span className="pt-2">Your Cart is Empty.</span>
+                      </div>
+                    )
+                    }
 
                     <div className="mt-8">
                       <div className="flow-root">
