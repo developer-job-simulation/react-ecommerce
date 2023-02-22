8ddb7cabb18bb6cc70dc82cb041721eb04d3257a
Matt Pauls <mattpauls@gmail.com>
2023-02-22 15:16:56 -0800

    load cart from localstorage if exists

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cart from "../Components/Cart";
 import NavBar from "../Components/NavBar";
 import ProductTable from "../Components/ProductTable";
 
 function Home() {
   const [open, setOpen] = useState(false);
-  const [cart, updateCart] = useState([]);
+  const [cart, updateCart] = useState(() => {
+    const localCart = JSON.parse(localStorage.getItem('cart'))
+    return localCart ? localCart : []
+  });
+
+  // Read cart from localstorage on load
+  // useEffect(() => {
+  //   const cart = JSON.parse(localStorage.getItem('cart'))
+  //   if (cart) {
+  //     updateCart(cart)
+  //   }
+  // }, [])
 
   return (
     <main>
