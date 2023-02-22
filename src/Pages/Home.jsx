bfdffeb0c56fa0d6266a84eed16ce2b0eccbe952
Matt Pauls <mattpauls@gmail.com>
2023-02-22 15:36:32 -0800

    update localstorage cart on add/remove item

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,13 +10,16 @@ function Home() {
     return localCart ? localCart : []
   });
 
-  // Read cart from localstorage on load
-  // useEffect(() => {
-  //   const cart = JSON.parse(localStorage.getItem('cart'))
-  //   if (cart) {
-  //     updateCart(cart)
-  //   }
-  // }, [])
+  // Trying to have one location to update the local storage when the cart state changes, but only when items are added or removed.
+  // It doesn't make sense to overwrite the localstorage every time the page is loaded with the logic I have with state, above.
+  // But this implementation isn't much better, since I'm reading from disk twice on first load, and then again every time the cart is updated.
+  // The alternative that I'm aware of is to put it in the onClick actions for adding and removing from the cart, which probably makes more sense at this point in time.
+  // That way, we're only reading/writing to disk as actually necessary, but it does spread the code to do the same thing across multiple components.
+  useEffect(() => {
+    if (cart != JSON.parse(localStorage.getItem('cart'))){
+      localStorage.setItem('cart', JSON.stringify(cart))
+    }
+  }, [cart])
 
   return (
     <main>
