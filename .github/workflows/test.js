5d53d82f5bf732cb2f1b0c4c5966fdaa6b830fd8
sbmsr <sebastian.messier@gmail.com>
2023-03-01 09:00:12 +0100

    finish tests

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -70,8 +70,120 @@ test("Solved Issue #2: Cart Persistence on Refresh", async () => {
 });
 
 test("Solved Issue #3: Product Filters", async () => {
-  await page.goto("http://localhost:3000");
-  assert.fail();
+  await page.goto("http://localhost:3000/");
+  await setTimeout(1000);
+  await page.getByRole("button", { name: "0 Filters" }).click();
+  await page.screenshot({ path: "/test.png" });
+
+  // $0 - $25 Filter
+  await page.getByLabel("$0 - $25").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Focus Paper Refill");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 1);
+  await page.getByLabel("$0 - $25").uncheck();
+
+  // $25 - $50 Filter
+  await page.getByLabel("$25 - $50").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Earthen Bottle");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 4);
+  await page.getByLabel("$25 - $50").uncheck();
+
+  // $50 - $75 Filter
+  await page.getByLabel("$50 - $75").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Everyday Ruck Snack");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 1);
+  await page.getByLabel("$50 - $75").uncheck();
+
+  // $75+ Filter
+  await page.getByLabel("$75+").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Basic Tee 6 Pack");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 2);
+  await page.getByLabel("$75+").uncheck();
+
+  // Beige Filter
+  await page.getByLabel("Beige").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Earthen Bottle");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 1);
+  await page.getByLabel("Beige").uncheck();
+
+  // Green Filter
+  await page.getByLabel("Green").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Nomad Tumbler");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 1);
+  await page.getByLabel("Green").uncheck();
+
+  // White Filter
+  await page.getByLabel("White").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Focus Paper Refill");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 2);
+  await page.getByLabel("White").uncheck();
+
+  // Black Filter
+  await page.getByLabel("Black").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Machined Mechanical Pencil");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 2);
+  await page.getByLabel("Black").uncheck();
+
+  // Gray Filter
+  await page.getByLabel("Gray").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Zip Tote Basket");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 1);
+  await page.getByLabel("Gray").uncheck();
+
+  // Teal Filter
+  await page.getByLabel("Teal").check();
+  var firstItemName = await page.$eval(
+    "#root > main > div:nth-child(2) > div > div > a:nth-child(1) > h3",
+    (el) => el.innerText
+  );
+  assert.is(firstItemName, "Everyday Ruck Snack");
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 1);
+  await page.getByLabel("Teal").uncheck();
+
+  // Compound filter
+  await page.getByLabel("$0 - $25").check();
+  await page.getByLabel("Beige").check();
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 0);
+
+  // Clear all filters
+  await page.getByRole("button", { name: "Clear all" }).click();
+  assert.is(await page.locator("#root > main > div:nth-child(2) > div > div > a").count(), 8);
+  await page.screenshot({ path: "/test.png" });
 });
 
 test("Solved Issue #4: Product Fetch is Causing Infinite Loop", async () => {
