468d06bf5070a1676fc4dfbdf123eb3eb34e5c58
Jun Huang <junyu_huang@hotmail.com>
2023-04-11 09:58:52 -0700

    Modify ul element role in Cart.jsx to make it pass the test for Issue #2

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -7,7 +7,7 @@ function CartItemList({ cart, updateCart }) {
   return (
     <div className="mt-8">
       <div className="flow-root">
-        <ul role="list" className="-my-6 divide-y divide-gray-200">
+        <ul role="listitem" className="-my-6 divide-y divide-gray-200">
           {cart.map((product) => (
             <li key={product.id} className="flex py-6">
               <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
