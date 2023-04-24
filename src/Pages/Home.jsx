190c4dabf4d7d87afdf84a4c501d67c0ea07dd52
dev_only <121720839+brudevtek@users.noreply.github.com>
2023-04-24 18:50:49 -0500

    solved issue 2,4,5,6,7,8.note:  1 and 3 remain unsolved

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
-import Cart from "../Components/Cart";
-import NavBar from "../Components/NavBar";
-import ProductTable from "../Components/ProductTable";
+import React, { useState } from 'react';
+import Cart from '../Components/Cart';
+import NavBar from '../Components/NavBar';
+import ProductTable from '../Components/ProductTable';
 
 function Home() {
   const [open, setOpen] = useState(false);
-  const [cart, updateCart] = useState([]);
+  const [cart, updateCart] = useState(JSON.parse(window.localStorage.getItem('local_storage_cart')) ||[]);
+  const [cartsubtotal, setCartSubTotal] = useState([]);
+
+
 
   return (
     <main>
-      <NavBar {...{ setOpen }} />
+      <NavBar {...{ setOpen, cart }} />
       <Cart {...{ open, setOpen, cart, updateCart }} />
-      <ProductTable {...{ cart, updateCart }} />
+      <ProductTable {...{ cart, updateCart,cartsubtotal}} />
     </main>
   );
 }
