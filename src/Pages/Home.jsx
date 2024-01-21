0b1ddc57c06d5a29fa3dd4cac47b97afa250febd
kefflen <kefflenjava@hotmail.com>
2024-01-20 22:35:07 -0300

    feat: implementing cart persistence on refresh

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cart from "../Components/Cart";
 import NavBar from "../Components/NavBar";
 import ProductTable from "../Components/ProductTable";
 
 function Home() {
   const [open, setOpen] = useState(false);
-  const [cart, updateCart] = useState([]);
+  const [cart, updateCart] = useState(() => {
+    const cart = localStorage.getItem("cart");
+    return cart ? JSON.parse(cart) : [];
+  });
+
+  useEffect(() => {
+    const cartJSON = JSON.stringify(cart);
+    localStorage.setItem("cart", cartJSON);
+  }, [cart])
 
   return (
     <main>
